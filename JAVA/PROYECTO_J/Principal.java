b8144f963c9f2a09527ad602fcb86f79787048c4
Andree <86030017+TheAndree7u7@users.noreply.github.com>
2024-07-20 21:46:14 -0500

    Se agregar el 1 feature

---
+++ JAVA/PROYECTO_J/Principal.java
@@ -0,0 +1,11 @@
+/**
+ * The Main class is the entry point of the Java program.
+ */
+public class Principal {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+    }
+}
