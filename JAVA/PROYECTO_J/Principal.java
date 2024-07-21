c1000dbe9de4d4c526e2c3a6812172d741c39718
Andree <86030017+TheAndree7u7@users.noreply.github.com>
2024-07-20 21:49:44 -0500

    Se agregar el 2 feature

--- JAVA/PROYECTO_J/Principal.java
+++ JAVA/PROYECTO_J/Principal.java
@@ -7,5 +7,12 @@ public class Principal {
         int num2 = 10;
         int sum = num1 + num2;
         System.out.println("The sum is: " + sum);
+
+        // Call the add() method
+        // Second feature
+        String name = "Hello World";
+        System.out.println(name);
+
     }
+
 }
